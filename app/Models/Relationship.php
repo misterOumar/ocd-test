ddc94fbb5c986c73e9bbefeb66f96dc7f0184ce2
misterOumar <coulibalyoumartc@gmail.com>
2025-02-08 11:44:59 +0100

    exercice 2 : creation des modeles eloquants correspondant et definition des relations

---
+++ app/Models/Relationship.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Relationship extends Model
+{
+    protected $fillable = [
+        'created_by',
+        'parent_id',
+        'child_id',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(People::class, 'parent_id');
+    }
+
+    public function child()
+    {
+        return $this->belongsTo(People::class, 'child_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
