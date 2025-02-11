826af1a289f6bf1103f729e28b5bbe6493e78e7f
misterOumar <coulibalyoumartc@gmail.com>
2025-02-12 00:27:40 +0100

    style detail d'une personne

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -1,27 +1,57 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+@endsection
+
 @section('content')
-    <h1>{{ $person->first_name }} {{ $person->last_name }}</h1>
+    <div class="content">
+        <div class="card">
 
-    <p><strong>Date de naissance :</strong> {{ $person->date_of_birth ?? 'Non renseignée' }}</p>
+            <div class="card-img">
+                <img src="{{ asset('assets/images/user-default.png') }}" alt="profile">
+            </div>
 
-    <h2>Parents</h2>
-    <ul>
-        @forelse($person->parents as $parent)
-            <li>{{ $parent->first_name }} {{ $parent->last_name }}</li>
-        @empty
-            <li>Aucun parent enregistré</li>
-        @endforelse
-    </ul>
+            <div class="desc">
+                <h1>{{ $person->first_name }} {{ $person->last_name }}</h1>
 
-    <h2>Enfants</h2>
-    <ul>
-        @forelse($person->children as $child)
-            <li>{{ $child->first_name }} {{ $child->last_name }}</li>
-        @empty
-            <li>Aucun enfant enregistré</li>
-        @endforelse
-    </ul>
+                <p><strong>Date de naissance :</strong> {{ $person->date_of_birth ?? 'Non renseignée' }}</p>
 
-    <a href="{{ route('people.index') }}" class="btn btn-secondary">Retour à la liste</a>
+            </div>
+
+            <div class="details">
+
+                <div class="parents">
+                    <h2>Parents  <i class='bx bx-child'></i></h2>
+                    <ul>
+                        @forelse($person->parents as $parent)
+                            <li>{{ $parent->first_name }} {{ $parent->last_name }}</li>
+                        @empty
+                            <li>Aucun parent enregistré</li>
+                        @endforelse
+                    </ul>
+
+                </div>
+
+                <div class="enfants">
+                    <h2>Enfants  <i class='bx bx-child'></i></h2>
+                    <ul>
+                        @forelse($person->children as $child)
+                            <li>{{ $child->first_name }} {{ $child->last_name }}</li>
+                        @empty
+                            <li>Aucun enfant enregistré</li>
+                        @endforelse
+                    </ul>
+
+                </div>
+
+
+            </div>
+
+
+
+            <a href="{{ route('people.index') }}" class="btn btn-secondary">Retour à la liste</a>
+        </div>
+
+    </div>
 @endsection
